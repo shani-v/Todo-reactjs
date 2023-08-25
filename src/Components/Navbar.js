9fc97fc643ba4b316aeb598d5f03e3e0b7f5de72
Shani <shanivish95@gmail.com>
2023-08-25 17:58:11 +0530

    navbar some changese

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -51,7 +51,7 @@ const Navbar = () => {
                   Home
                 </Link>
               </li>
-              <li>
+              {/* <li>
                 <Link
                   to='/about'
                   className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
@@ -82,7 +82,7 @@ const Navbar = () => {
                 >
                   Contact
                 </Link>
-              </li>
+              </li> */}
             </ul>
           </div>
         </div>
